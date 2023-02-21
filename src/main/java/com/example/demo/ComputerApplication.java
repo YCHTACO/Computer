4cee31e245c2f3b2e03d9f1535c9fa38d1ae73c5
YCHTACO <124900245+YCHTACO@users.noreply.github.com>
2023-02-21 17:42:24 +0800

    in2

--- src/main/java/com/example/demo/ComputerApplication.java
+++ src/main/java/com/example/demo/ComputerApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,38 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ComputerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComputerApplication.class, args);
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("請輸入第一個數字:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("請輸入第二個數字:");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("請輸入運算符號 (+, -, *, /) :");
+        String operator = scanner.next();
+
+        double result = 0;
+
+        switch (operator) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("不支援的運算符號");
+                return;
+        }
+
+        System.out.println("計算結果是: " + result);
 	}
 
 }
